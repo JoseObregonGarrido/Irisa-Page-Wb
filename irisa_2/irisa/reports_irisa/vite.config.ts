d1a7f1c3e88c1a98bf55ca7b6c2d942702ffe0bd
Jose Obregon <joseobregon270@gmail.com>
2026-02-16 20:09:09 -0500

    Creacion del docer
    Creacion del docker para frontend.

--- irisa_2/irisa/reports_irisa/vite.config.ts
+++ irisa_2/irisa/reports_irisa/vite.config.ts
@@ -4,8 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-  base:"./",
+  base: "./",
+  server: {
+    host: true,        // Esto es igual a 0.0.0.0, permite conexiones externas
+    port: 5173,
+    watch: {
+      usePolling: true // Necesario para que detecte cambios de archivos en Windows (WSL/Docker)
+    },
+    hmr: {
+      clientPort: 5173 // Asegura que el navegador sepa a qué puerto conectar el WebSocket
+    }
+  }
 })
