b5523793e8972b55d94b965d76eaac3afcc51f38
Jose Obregon <joseobregon270@gmail.com>
2026-03-04 20:54:19 -0500

    AÑADIR ERROR MA EN TABLA MA
    AÑADIR ERROR MA EN TABLA MA
    8:54 FINAL DIA 4/ 4

--- irisa_2/irisa/reports_irisa/src/components/trasmitter/InputField.tsx
+++ irisa_2/irisa/reports_irisa/src/components/trasmitter/InputField.tsx
@@ -1,6 +1,6 @@
 export const InputField = ({ label, value, onChange, unit, isError = false, readOnly = false }: any) => {
     const safeValue = value ?? "";
-    const inputWidth = Math.max(safeValue.toString().length, 6) + 5
+    const inputWidth = Math.max(safeValue.toString().length, 6) + 5;
     return (
         <div className="flex flex-col min-w-fit group">
             <label className="block text-[9px] font-black text-gray-400 uppercase mb-1 tracking-wider whitespace-nowrap group-focus-within:text-teal-600 transition-colors">
@@ -8,7 +8,7 @@ export const InputField = ({ label, value, onChange, unit, isError = false, read
             </label>
             <div className="relative inline-flex items-center">
                 <input
-                    type="text" value={value} onChange={onChange} readOnly={readOnly}
+                    type="text" value={safeValue} onChange={onChange} readOnly={readOnly}
                     style={{ width: `${inputWidth}ch` }} 
                     className={`px-3 py-2 pr-8 text-xs border rounded-lg focus:outline-none focus:ring-2 transition-all
                         ${isError ? 'border-red-200 bg-red-50 focus:ring-red-500 font-bold text-red-700' 
