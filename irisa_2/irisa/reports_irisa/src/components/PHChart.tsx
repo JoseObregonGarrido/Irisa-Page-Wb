73564ce4e522c5127f3abf697042502a2011dde5
Jose Obregon <joseobregon270@gmail.com>
2026-03-06 11:03:21 -0500

    WAAAAAOOOOOOOO,
    11: 02  WAO

--- irisa_2/irisa/reports_irisa/src/components/PHChart.tsx
+++ irisa_2/irisa/reports_irisa/src/components/PHChart.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import {
     LineChart, Line, BarChart, Bar,
     XAxis, YAxis, CartesianGrid, Tooltip,
